4385d7d158f6403d16dfdcb718bd56d033f38fbf
GunYulePark <85776027+GunYulePark@users.noreply.github.com>
2023-03-10 15:03:23 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/opt/Ringca
 cd $REPOSITORY
 
-APP_NAME=action_codedeploy
+APP_NAME=RingCa_deploy_2
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
